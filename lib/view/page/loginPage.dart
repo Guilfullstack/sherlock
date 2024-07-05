98e5eb49594166c5df29febb42a54d05c64cb7cf
Guilherme Almeida <guidevtec@gmail.com>
2024-07-05 17:18:40 -0300

    Alterações login

--- lib/view/page/loginPage.dart
+++ lib/view/page/loginPage.dart
@@ -1,52 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:sherlock/controller/user_controller.dart';
 import 'package:sherlock/model/user_adm.dart';
 import 'package:sherlock/model/user_team.dart';
+import 'package:sherlock/view/widgets/imput_text.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({super.key});
   UserController userController = UserController();
   @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>(); // Chave para o formulário
+
     return Scaffold(
       backgroundColor: Colors.black87,
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            //Expanded(child: Container()),
-            Image.asset(
-              'images/logo.png',
-              //width: 100,
-              // height: 100,
-            ),
-            TextFormField(
-              controller: userController.email,
-            ),
-            TextFormField(
-              controller: userController.password,
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () async {},
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              Image.asset(
+                'images/logo.png',
+              ),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    ImputTextFormField(
+                        title: "Login", controller: userController.login),
+                    const SizedBox(height: 20),
+                    ImputTextFormField(
+                        title: "Senha", controller: userController.password),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              FractionallySizedBox(
+                widthFactor: 0.5,
+                child: ElevatedButton(
+                  onPressed: () async {
+                    // Verifica se o formulário é válido
+                    if (_formKey.currentState!.validate()) {
+                      String login = userController.login.text;
+                      String password = userController.password.text;
+
+                      print('Login: $login, Password: $password');
+                    }
+                    userController.loginSystem(
+                        context,
+                        userController.login.text,
+                        userController.password.text);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.purple,
+                    foregroundColor: Colors.white,
+                    textStyle: TextStyle(fontSize: 20),
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                  ),
                   child: const Text('Login'),
                 ),
-                ElevatedButton(
-                  onPressed: () async {
-                    print(userController.email.text);
-                    print(userController.password.text);
-                    UserTeam newAdm = UserTeam(
-                        login: userController.email.text,
-                        password: userController.password.text);
-                    userController.addUserTeam(newAdm);
-                  },
-                  child: const Text('Cadastro'),
-                ),
-              ],
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/view/widgets/imput_text.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class ImputTextFormField extends StatelessWidget {
+  final String title;
+  final TextEditingController controller;
+
+  ImputTextFormField({required this.title, required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      widthFactor: 0.7, // Define 50% da largura da tela
+      child: TextFormField(
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Por favor, preencha este campo';
+          }
+          return null;
+        },
+        controller: controller,
+        style: const TextStyle(color: Colors.white),
+        decoration: InputDecoration(
+          labelText: title,
+          labelStyle: const TextStyle(color: Colors.white),
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.purple),
+          ),
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.purple),
+          ),
+          border: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.purple),
+          ),
+        ),
+      ),
+    );
+  }
+}
