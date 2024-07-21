64cc09d6cc8c2b4a55225e616c261042a4fad575
Italo <italof363@gmail.com>
2024-07-20 22:35:49 -0300

    preparado class stage e code

--- lib/model/stage.dart
+++ lib/model/stage.dart
@@ -1,11 +1,39 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:cloud_firestore/cloud_firestore.dart';
+final userStageRef = FirebaseFirestore.instance
+    .collection("Stage")
+    .withConverter<Stage>(
+        fromFirestore: (snapshots, _) => Stage.fromJson(snapshots.data()!),
+        toFirestore: (userStage, _) => userStage.toJson());
 class Stage {
+  String id;
   String token;
   String description;
   bool isUnlocked;
+  DateTime? date;
   Stage({
+    required this.id,
     required this.token,
     required this.description,
     required this.isUnlocked,
+    this.date,
   });
+
+  factory Stage.fromJson(Map<String, dynamic> json) {
+    return Stage(
+      id: json["id"],
+      token: json["token"],
+      description: json["description"],
+      isUnlocked: json['isUnlocked'],
+      date: json["date"] != null ? (json["date"] as Timestamp).toDate() : null,
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "token": token,
+        "description": description,
+        'isUnlocked':isUnlocked,
+        "date": date != null ? Timestamp.fromDate(date!) : null,
+      };
 }
