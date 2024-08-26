da955ddea96bd96881acff1e3cfcc314eb5d3fa7
Guilherme Almeida <guidevtec@gmail.com>
2024-08-25 23:34:52 -0300

    adicionando funcionalidade ao card de prova

--- lib/view/widgets/card_stage.dart
+++ lib/view/widgets/card_stage.dart
@@ -15,42 +15,95 @@ class CardStage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(8),
-      padding: const EdgeInsets.all(12),
-      decoration: BoxDecoration(
-        color: Colors.grey[800],
-        borderRadius: BorderRadius.circular(10),
-        border: Border.all(
-          color: Colors.white,
-          width: 1.0,
-        ),
-      ),
-      child: ListTile(
-        title: Text(
-          stage.description ?? '',
-          style: const TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
+    return InkWell(
+      onTap: isUnlocked
+          ? () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    backgroundColor: Colors.white,
+                    contentPadding: EdgeInsets.zero,
+                    content: Container(
+                      width: 300, // Define a largura do container
+                      height: 400, // Define a altura do container
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(
+                              'images/eniguima2.jpg'), // Caminho da imagem
+
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              "Enigma",
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                            const SizedBox(height: 20),
+                            Text(
+                              stage.puzzle ?? '',
+                              style: const TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+          : null,
+      child: Container(
+        margin: const EdgeInsets.all(8),
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: Colors.grey[800],
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
             color: Colors.white,
+            width: 1.0,
           ),
         ),
-        leading: CircleAvatar(
-          backgroundColor: Colors.grey,
-          child: Text(
-            '$position',
-            style: const TextStyle(color: Colors.white, fontSize: 18),
+        child: ListTile(
+          title: Text(
+            stage.description ?? '',
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
           ),
+          leading: CircleAvatar(
+            backgroundColor: Colors.grey,
+            child: Text(
+              '$position',
+              style: const TextStyle(color: Colors.white, fontSize: 18),
+            ),
+          ),
+          trailing: isUnlocked
+              ? const Icon(
+                  Icons.check_circle,
+                  color: Colors.green,
+                )
+              : const Icon(
+                  Icons.lock,
+                  color: Colors.red,
+                ),
         ),
-        trailing: isUnlocked
-            ? const Icon(
-                Icons.check_circle,
-                color: Colors.green,
-              )
-            : const Icon(
-                Icons.lock,
-                color: Colors.red,
-              ),
       ),
     );
   }
